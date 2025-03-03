bfe76f454eb9d8e35ad22da4b44c725a367c81d2
GibboGibby <james@netcom.co.uk>
2025-03-03 09:10:56 +0000

    Added a global settings struct that can be used to control all the settings needed for the project.

---
+++ DesktopSailboat/Settings.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Settings
+{
+	int particleCount = 20000;
+	float viscosity = 5.0f;
+};
+
+extern Settings g_Settings;
+
+inline void SaveSettings()
+{
+
+}
+
+inline void LoadSettings()
+{
+
+}
